ea33c0d117629cddba6c5daccd5a6904a9951198
Won9012 <chldnjsdy2@gmail.com>
2023-12-22 09:13:52 +0900

    [fix] 스크립트수정

--- Assets/3.Script/Inventory/ItemSlotUI.cs
+++ Assets/3.Script/Inventory/ItemSlotUI.cs
@@ -28,4 +28,6 @@ public abstract class ItemSlotUI : MonoBehaviour, IDropHandler
     public abstract void UpdateSlotUI();
     
     protected virtual void EnableSlotUI(bool enable) => itemIconImage.enabled = enable;
+
+
 }
